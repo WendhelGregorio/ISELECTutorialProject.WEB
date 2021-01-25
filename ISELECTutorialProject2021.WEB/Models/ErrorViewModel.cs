7f6716606c8ac12a7a6b26a7318f50574df2e087
Wendhel Gregorio <gregorio.wendhel@gmail.com>
2021-01-25 10:50:59 +0800

    Create Project

---
+++ ISELECTutorialProject2021.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ISELECTutorialProject2021.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
